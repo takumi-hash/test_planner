96d840de5c522ed3636e9841d65584f5dce3c0cf
Takumi Hashimoto <39505885+takumi-hash@users.noreply.github.com>
2019-05-28 20:27:37 +0900

    R integration pwr function added.

--- public/prior.R
+++ public/prior.R
@@ -1,9 +1,15 @@
-library(rjson)
-library(pwr)
+library(rjson);
+library(pwr);
 
-x1 <- commandArgs()[5]
+
+x1 <- commandArgs()[5];
 
 xx1 <- fromJSON(x1);
-result <- pwr.t.test(d=xx1['effect_size'], sig.level=xx1['p_value'], power=xx1['power'] );
-tmp <- getwd();
-cat(toJSON(c(xx1)));
+
+effect_size <- as.numeric(xx1["effect_size"]);
+power <- as.numeric(xx1["power"]);
+p_value <- as.numeric(xx1["p_value"]);
+
+result <- pwr.t.test(d = effect_size, power = power, sig.level = p_value);
+xx1["result_n"] <- result$n;
+cat(toJSON(c(xx1["result_n"])));
