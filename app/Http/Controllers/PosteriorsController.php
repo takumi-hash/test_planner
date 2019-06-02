5d380d47c11d5c6e6a0b2bb5ced6f0d665ccf4f6
Takumi Hashimoto <39505885+takumi-hash@users.noreply.github.com>
2019-06-02 21:35:46 +0900

    r path set for heroku

--- app/Http/Controllers/PosteriorsController.php
+++ app/Http/Controllers/PosteriorsController.php
@@ -32,7 +32,7 @@ class PosteriorsController extends Controller
         $param = array('control_imp' => $control_imp, 'experimental_imp' => $experimental_imp, 'control_cv' => $control_cv, 'experimental_cv' => $experimental_cv);
         $encParam = json_encode($param);
 
-        $cmd = "R --vanilla --slave --args '$encParam' < posterior.R";
+        $cmd = "~/bin/R --vanilla --slave --args '$encParam' < posterior.R";
         exec($cmd, $response);
         $res = $response[0];
 
