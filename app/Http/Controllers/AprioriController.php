cd837b063c25e100bbeccd3632a7e2d931cdced4
橋本拓実 <takumi@hashimototakuminoiMac.local>
2020-05-09 19:13:19 +0900

    ApriorController for API response added.

---
+++ app/Http/Controllers/AprioriController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AprioriController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Display a calculated result of the resource based upon parameters provided with Request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function calc(Request $request){
+      $control_ctr = $request->control_ctr;
+      $experimental_ctr = $request->experimental_ctr;
+      $p_value = $request->p_value;
+      $power = $request->power;
+
+      $param = array('control_ctr' => $control_ctr, 'experimental_ctr' => $experimental_ctr, 'p_value' => $p_value, 'power' => $power);
+      $encParam = json_encode($param);
+
+      //prod $cmd = "~/bin/R --vanilla --slave --args '$encParam' < prior.R";
+      $cmd = "/usr/local/bin/R --vanilla --slave --args '$encParam' < prior.R";
+      exec($cmd, $response);
+      $res = $response[0];
+
+      return [
+          'p_value' => $p_value,
+          'power' => $power,
+          'res' => json_decode($res),
+      ];
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request /* , $id */)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
