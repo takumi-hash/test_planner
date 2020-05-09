82cc1f82a9ee17f995beca47866ac7c53bae4013
橋本拓実 <takumi@hashimototakuminoiMac.local>
2020-05-09 21:27:34 +0900

    Aprioti Controller added.

---
+++ app/Http/Controllers/AprioriController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AprioriController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $control_ctr = $request->control_ctr;
+        $experimental_ctr = $request->experimental_ctr;
+        $p_value = $request->p_value;
+        $power = $request->power;
+
+        $param = array('control_ctr' => $control_ctr, 'experimental_ctr' => $experimental_ctr, 'p_value' => $p_value, 'power' => $power);
+        $encParam = json_encode($param);
+
+        $cmd = "~/bin/R --vanilla --slave --args '$encParam' < prior.R";
+        // dev $cmd = "/usr/local/bin/R --vanilla --slave --args '$encParam' < prior.R";
+        exec($cmd, $response);
+        $res = $response[0];
+
+        return ['res' => json_decode($res),];
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request /* , $id */)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
